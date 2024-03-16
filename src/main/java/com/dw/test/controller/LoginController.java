d5106918d5a38952142f69c80592a7beb42e565d
canghaiwolong666 <17316088308@163.com>
2024-03-16 20:15:52 +0800

    第4次提交

--- src/main/java/com/dw/test/controller/LoginController.java
+++ src/main/java/com/dw/test/controller/LoginController.java
@@ -1,15 +1,19 @@
 package com.dw.test.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
+@Slf4j
 public class LoginController {
 
 
     @RequestMapping("/login")
     public String login(){
+        log.info("开始登录了。。。。。。。。。。。。。。");
         return "登錄成功";
     }
 }
